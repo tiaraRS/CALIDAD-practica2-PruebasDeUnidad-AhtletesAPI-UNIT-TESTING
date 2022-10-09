f701fb7b45635b88629445ac0428d75e9db58c7c
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-10-08 22:53:42 -0400

    DisciplineService.GetDisciplinesAsync ut added

---
+++ UnitTesting/ServicesUT/DisciplineServiceUT.cs
@@ -0,0 +1,42 @@
+﻿using AthletesRestAPI.Data.Entity;
+using AthletesRestAPI.Data.Repository;
+using AthletesRestAPI.Services;
+using AutoMapper;
+using Moq;
+using RestaurantRestAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.ServicesUT
+{
+    public class DisciplineServiceUT
+    {
+        [Fact]
+        public async Task GetDisciplinesAsync_ReturnsListOfDisciplines()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<AutomapperProfile>());
+            var mapper = config.CreateMapper();
+            var disciplineEntity100M = new DisciplineEntity()
+            {
+                Id = 1,
+                Name = "100M"              
+            };
+            var disciplineEntity200M = new DisciplineEntity()
+            {
+                Id = 1,
+                Name = "200M"               
+            };
+            var disciplinesEnumerable = new List<DisciplineEntity>() { disciplineEntity100M, disciplineEntity200M } as IEnumerable<DisciplineEntity>;
+            var repositoryMock = new Mock<IAthleteRepository>();
+            repositoryMock.Setup(r => r.GetDisciplinesAsync()).ReturnsAsync(disciplinesEnumerable);
+
+            var disciplinesService = new DisciplineService(repositoryMock.Object, mapper);
+            var disciplinesList = await disciplinesService.GetDisciplinesAsync();
+            Assert.NotEmpty(disciplinesList);
+            Assert.Equal(2,disciplinesList.Count());
+        }
+    }
+}
