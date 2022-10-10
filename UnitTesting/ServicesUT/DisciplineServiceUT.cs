b8e551ce134dafb3543a1a03a0796715a1caf212
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-10-10 14:24:45 -0400

    DisciplineService.GetDisciplineAsync ut added

--- UnitTesting/ServicesUT/DisciplineServiceUT.cs
+++ UnitTesting/ServicesUT/DisciplineServiceUT.cs
@@ -1,5 +1,7 @@
 ﻿using AthletesRestAPI.Data.Entity;
 using AthletesRestAPI.Data.Repository;
+using AthletesRestAPI.Exceptionss;
+using AthletesRestAPI.Models;
 using AthletesRestAPI.Services;
 using AutoMapper;
 using Moq;
@@ -37,6 +39,41 @@ namespace UnitTesting.ServicesUT
             var disciplinesList = await disciplinesService.GetDisciplinesAsync();
             Assert.NotEmpty(disciplinesList);
             Assert.Equal(2,disciplinesList.Count());
+        }
+
+
+        [Fact]
+        public async Task GetDisciplineAsync_ValidId_ReturnsDisciplineCorrespondingToId()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<AutomapperProfile>());
+            var mapper = config.CreateMapper();
+            var disciplineEntity100M = new DisciplineEntity()
+            {
+                Id = 1,
+                Name = "100M"
+            };           
+            var repositoryMock = new Mock<IAthleteRepository>();
+            repositoryMock.Setup(r => r.GetDisciplineAsync(1, false)).ReturnsAsync(disciplineEntity100M);
+            var disciplinesService = new DisciplineService(repositoryMock.Object, mapper);
+            var disciplineFromDB = await disciplinesService.GetDisciplineAsync(1);
+
+            Assert.NotNull(disciplineFromDB);
+            Assert.True("100M" == disciplineFromDB.Name);
+            Assert.IsType<DisciplineModel>(disciplineFromDB);
+        }
+        //24.5%
+        [Fact]
+        public async Task GetDisciplineAsync_InvalidId_ThrowsNotFoundElementException()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<AutomapperProfile>());
+            var mapper = config.CreateMapper();
+            var repositoryMock = new Mock<IAthleteRepository>();
+            repositoryMock.Setup(r => r.GetDisciplineAsync(100, false)).ReturnsAsync((DisciplineEntity)null);
+            var disciplinesService = new DisciplineService(repositoryMock.Object, mapper);
+
+            var exception = Assert.ThrowsAsync<NotFoundElementException>(async () => await disciplinesService.GetDisciplineAsync(100));
+            Assert.Equal("discipline with id 100 does not exist", exception.Result.Message);
+
         }
     }
 }
