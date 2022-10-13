f32d740c0422b823cb8106b359cbbe5c50e90691
Diego Quiñones <diegoqr2000@gmail.com>
2022-10-13 08:29:56 -0400

    Added tests for LoginUserAsync

---
+++ UnitTesting/ServicesUT/UserServiceUT.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AthletesRestAPI.Data.Entity;
+using AthletesRestAPI.Data.Repository;
+using AthletesRestAPI.Exceptionss;
+using AthletesRestAPI.Models;
+using AthletesRestAPI.Models.Security;
+using AthletesRestAPI.Services;
+using AthletesRestAPI.Services.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using RestaurantRestAPI.Data;
+
+namespace UnitTesting.ServicesUT
+{
+    public class UserServiceUT
+    {
+        //LoginUserAsync
+        //tc1
+        [Fact]
+        public async Task LoginUser_DoesntFoundUserWithEmail()
+        {
+            //ARRANGE
+            var loginModel = new LoginViewModel()
+            {
+                Email = "testmail@gmai.com",
+                Password = "Secret1234",
+            };
+
+            var userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+            var roleManagerMock = new Mock<RoleManager<IdentityRole>>(Mock.Of<IRoleStore<IdentityRole>>(), null, null, null, null);
+            var configurationMock = new Mock<IConfiguration>();
+
+            userManagerMock.Setup(r => r.FindByEmailAsync(loginModel.Email)).ReturnsAsync((IdentityUser)null);
+
+            //ACT
+            var userService = new UserService(userManagerMock.Object, roleManagerMock.Object, configurationMock.Object);
+            var response = await userService.LoginUserAsync(loginModel);
+
+            //ASSERT
+            Assert.Equal("There is no user with that Email address", response.Token);
+        }
+
+        //tc2
+        [Fact]
+        public async Task LoginUser_UserInvalidPassword()
+        {
+            //ARRANGE
+            var loginModel = new LoginViewModel()
+            {
+                Email = "testmail@gmai.com",
+                Password = "thisIsNotThePass",
+            };
+            var identityUser = new IdentityUser()
+            {
+                Id = "12",
+                UserName = "TestName",
+                Email = "testmail@gmai.com",
+                PasswordHash = "hasAFAS2341HES",
+            };
+            var userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+            var roleManagerMock = new Mock<RoleManager<IdentityRole>>(Mock.Of<IRoleStore<IdentityRole>>(), null, null, null, null);
+            var configurationMock = new Mock<IConfiguration>();
+
+            userManagerMock.Setup(r => r.FindByEmailAsync(loginModel.Email)).ReturnsAsync(identityUser);
+            userManagerMock.Setup(r => r.CheckPasswordAsync(identityUser, loginModel.Password)).ReturnsAsync(false);
+
+            //ACT
+            var userService = new UserService(userManagerMock.Object, roleManagerMock.Object, configurationMock.Object);
+            var response = await userService.LoginUserAsync(loginModel);
+
+            //ASSERT
+            Assert.Equal("Invalid password", response.Token);
+        }
+
+        //tc3
+        [Fact]
+        public async Task LoginUser_LoginIsSuccesfulWithRoles()
+        {
+            //ARRANGE
+            var loginModel = new LoginViewModel()
+            {
+                Email = "correctMail@gmail.com",
+                Password = "Secret1234",
+            };
+            var identityUser = new IdentityUser()
+            {
+                Id = "12",
+                UserName = "TestName",
+                Email = "correctMail@gmail.com",
+                PasswordHash = "hasAFAS2341HES",
+            };
+            var roles = new List<String>();
+            roles.Add("Admin");
+            roles.Add("User");
+            var userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+            var roleManagerMock = new Mock<RoleManager<IdentityRole>>(Mock.Of<IRoleStore<IdentityRole>>(), null, null, null, null);
+            var configurationMock = new Mock<IConfiguration>();
+
+            userManagerMock.Setup(r => r.FindByEmailAsync(loginModel.Email)).ReturnsAsync(identityUser);
+            userManagerMock.Setup(r => r.CheckPasswordAsync(identityUser, loginModel.Password)).ReturnsAsync(true);
+            userManagerMock.Setup(r => r.GetRolesAsync(identityUser)).ReturnsAsync(roles);
+            configurationMock.Setup(r => r["AuthSettings:Key"]).Returns("SuperSecretKeyEncryptor");
+
+            //ACT
+            var userService = new UserService(userManagerMock.Object, roleManagerMock.Object, configurationMock.Object);
+            var response = await userService.LoginUserAsync(loginModel);
+
+            //ASSERT
+            Assert.True(response.IsSuccess);
+        }
+
+        //tc3
+        [Fact]
+        public async Task LoginUser_LoginIsSuccesfulWithoutRoles()
+        {
+            //ARRANGE
+            var loginModel = new LoginViewModel()
+            {
+                Email = "correctMail@gmail.com",
+                Password = "Secret1234",
+            };
+            var identityUser = new IdentityUser()
+            {
+                Id = "12",
+                UserName = "TestName",
+                Email = "correctMail@gmail.com",
+                PasswordHash = "hasAFAS2341HES",
+            };
+            var roles = new List<String>();
+            var userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+            var roleManagerMock = new Mock<RoleManager<IdentityRole>>(Mock.Of<IRoleStore<IdentityRole>>(), null, null, null, null);
+            var configurationMock = new Mock<IConfiguration>();
+
+            userManagerMock.Setup(r => r.FindByEmailAsync(loginModel.Email)).ReturnsAsync(identityUser);
+            userManagerMock.Setup(r => r.CheckPasswordAsync(identityUser, loginModel.Password)).ReturnsAsync(true);
+            userManagerMock.Setup(r => r.GetRolesAsync(identityUser)).ReturnsAsync(roles);
+            configurationMock.Setup(r => r["AuthSettings:Key"]).Returns("SuperSecretKeyEncryptor");
+
+            //ACT
+            var userService = new UserService(userManagerMock.Object, roleManagerMock.Object, configurationMock.Object);
+            var response = await userService.LoginUserAsync(loginModel);
+
+            //ASSERT
+            Assert.True(response.IsSuccess);
+        }
+    }
+}
